ddd6feb09fe290c3ff832628515a78668614528a
nutik734 <srta.anna.kina@gmail.com>
2023-05-14 23:30:09 +0200

    added icon

--- src/app.js
+++ src/app.js
@@ -21,24 +21,28 @@ function formatDate(timestamp){
 }
 
 function displayTemperature(response){
-console.log(response.data.temperature.current);
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML= response.data.city;
-let temperatureElement = document.querySelector("#temperature");
-temperatureElement.innerHTML= Math.round(response.data.temperature.current);
-let descriptionElement = document.querySelector("#description");
-descriptionElement.innerHTML= response.data.condition.description;
-let humidity= document.querySelector("#humidity");
-humidity.innerHTML= response.data.temperature.humidity;
-let wind= document.querySelector("#wind");
-wind.innerHTML= response.data.wind.speed;
-let pressure = document.querySelector("#pressure");
-pressure.innerHTML= response.data.temperature.pressure;
-let date = document.querySelector("#date");
-date.innerHTML= formatDate(response.data.time*1000);
+   let cityElement = document.querySelector("#city");
+   cityElement.innerHTML= response.data.city;
+   let temperatureElement = document.querySelector("#temperature");
+   temperatureElement.innerHTML= Math.round(response.data.temperature.current);
+   let descriptionElement = document.querySelector("#description");
+   descriptionElement.innerHTML= response.data.condition.description;
+   let humidity= document.querySelector("#humidity");
+   humidity.innerHTML= response.data.temperature.humidity;
+   let wind= document.querySelector("#wind");
+   wind.innerHTML= response.data.wind.speed;
+   let pressure = document.querySelector("#pressure");
+   pressure.innerHTML= response.data.temperature.pressure;
+   let date = document.querySelector("#date");
+   date.innerHTML= formatDate(response.data.time*1000);
+   let iconElement= document.querySelector("#icon");
+   iconElement.setAttribute("src", `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
+   iconElement.setAttribute("alt", response.data.condition.description);
+
 }
 
 let apiKey="0a49584f932f33a5d9ea5beto34a414d";
-let apiUrl= `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`;
+let query= "Barcelona";
+let apiUrl= `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
