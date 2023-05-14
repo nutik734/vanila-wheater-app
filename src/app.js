f0026d40b8f288f9e21017334de643c5c37a626d
nutik734 <srta.anna.kina@gmail.com>
2023-05-14 22:49:07 +0200

    time updated

--- src/app.js
+++ src/app.js
@@ -1,3 +1,25 @@
+function formatDate(timestamp){
+    let date= new Date(timestamp);
+    let hours= date.getHours();
+      if (hours < 10) {
+     hours = `0${hours}`;
+     }
+    let minutes= date.getMinutes();
+      if (minutes < 10) {
+     minutes = `0${minutes}`;
+      }
+    let days= 
+    ["Sunday", 
+     "Monday", 
+     "Tuesday", 
+     "Wednesday",
+     "Thursday", 
+     "Friday", 
+     "Saturday"];
+    let day= days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response){
 console.log(response.data.temperature.current);
 let cityElement = document.querySelector("#city");
@@ -12,6 +34,8 @@ let wind= document.querySelector("#wind");
 wind.innerHTML= response.data.wind.speed;
 let pressure = document.querySelector("#pressure");
 pressure.innerHTML= response.data.temperature.pressure;
+let date = document.querySelector("#date");
+date.innerHTML= formatDate(response.data.time*1000);
 }
 
 let apiKey="0a49584f932f33a5d9ea5beto34a414d";
